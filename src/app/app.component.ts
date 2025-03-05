22eea87f06c1d9de8b29f33b2b08c2fac71a9e08
Keveen Menezes <endykeven@gmail.com>
2025-03-05 00:16:19 -0300

    implement product listing with pagination and add product model types

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./layout/header/header.component";
+import { HeaderComponent } from './layout/header/header.component';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'eShop';
+export class AppComponent implements OnInit {
+  private readonly baseUrl = 'https://localhost:5050/api/';
+  private readonly http = inject(HttpClient);
+  private readonly title = 'eShop';
+  products: Product[] = [];
+  pageIndex = 1;
+  pageSize = 10;
+
+  ngOnInit(): void {
+    this.http
+      .get<Pagination<Product>>(
+        `${this.baseUrl}products/${this.pageIndex}/${this.pageSize}`
+      )
+      .subscribe({
+        next: (response) => {
+          this.products = response.items;
+        },
+        error: (error: any) => {
+          console.error(error);
+        },
+        complete: () => {
+          console.log('complete');
+        },
+      });
+  }
 }
---
+++ src/app/shared/models/product.ts
@@ -0,0 +1,8 @@
+export type Product = {
+  id: string;
+  name: string;
+  description: string;
+  imageFile: string;
+  price: number;
+  categories: string[];
+};
